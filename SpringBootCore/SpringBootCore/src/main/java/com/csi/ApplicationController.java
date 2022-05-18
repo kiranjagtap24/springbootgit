aa9086946db23b94939517e66c91e739c5b119c2
kiranjagtap24 <kjagtap2414@gmail.com>
2022-05-18 13:02:04 +0530

    initial commit for spring boot app

---
+++ SpringBootCore/SpringBootCore/src/main/java/com/csi/ApplicationController.java
@@ -0,0 +1,20 @@
+package com.csi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+    @GetMapping
+    public String sayHello(){
+        return "Welcome to CSI PUNE!!";
+    }
+
+    @GetMapping("/services")
+    public String services(){
+        return "Software Development";
+    }
+}
+
